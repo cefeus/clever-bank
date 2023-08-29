735af1ea22c20caa0aac50cba390f5469ce741ca
cefe!s <fireinspirer@gmail.com>
2023-08-29 12:35:40 +0300

    add c3po connection pool; config connection

---
+++ src/main/java/config/db/C3p0Configuration.java
@@ -0,0 +1,39 @@
+package config.db;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import utils.PropertiesUtil;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class C3p0Configuration {
+    private final String URL = "url";
+    private final String USERNAME = "username";
+    private final String PASSWORD = "password";
+    private final String DRIVER = "driver";
+
+    private final ComboPooledDataSource connPool;
+
+    public C3p0Configuration() {
+        connPool = new ComboPooledDataSource();
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        connPool.setJdbcUrl(PropertiesUtil.getPropertyByKey(URL));
+        connPool.setUser(PropertiesUtil.getPropertyByKey(USERNAME));
+        connPool.setPassword(PropertiesUtil.getPropertyByKey(PASSWORD));
+        try {
+            connPool.setDriverClass(PropertiesUtil.getPropertyByKey(DRIVER));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Connection open() throws SQLException {
+        return connPool.getConnection();
+    }
+    public void close() {
+        connPool.close();
+    }
+}
---
+++ src/main/java/utils/PropertiesUtil.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesUtil {
+    private static final Properties PROP = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    private static void loadProperties() {
+        try (InputStream input = PropertiesUtil.class.
+                getClassLoader().
+                getResourceAsStream("application.yml")) {
+            PROP.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String getPropertyByKey(String key) {
+        return PROP.getProperty(key);
+    }
+}
